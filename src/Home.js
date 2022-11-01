86045b6f2ecade3c3ed9870fc65f37f972d281ad
Nancy Zhang <nancy.zhang143@gmail.com>
2022-11-01 11:00:55 -0400

    fix build

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import Container from '@mui/material/Container';
 import Box from '@mui/material/Box';
-import Chip from '@mui/material/Chip';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
